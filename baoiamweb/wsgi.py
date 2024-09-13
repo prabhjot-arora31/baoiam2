2d4869a8ac501336cc9cbed24643ae05f4db09f0
prabhjot-arora31 <prabhjotarora99@gmail.com>
2024-09-13 18:26:08 +0530

    lets see now

--- baoiamweb/wsgi.py
+++ baoiamweb/wsgi.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baoiamweb.settings')
-
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 application = get_wsgi_application()
 app  = application
